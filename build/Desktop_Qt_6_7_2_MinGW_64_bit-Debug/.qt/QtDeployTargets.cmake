e9f753719efde6f1d51117c93fcddda643af421d
CypherSoldier <calebwagner200@gmail.com>
2025-02-18 20:23:07 +0200

    Removed build folder from version control

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Text_Editor_FILE C:/Users/caleb/Documents/TextEditor/Text_Editor/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/Text_Editor.exe)
-set(__QT_DEPLOY_TARGET_Text_Editor_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Text_Editor_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
